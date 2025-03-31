9ed6be5a68ab32adeae0e5713bf6b003883c8a94
Vert <73512586+VertUnderscore@users.noreply.github.com>
2025-03-30 20:21:00 -0400

    added MIT license to this

--- google_calendar_integration.py
+++ google_calendar_integration.py
@@ -9,8 +9,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-
 class GoogleCalendar:
     def __init__(self):
         SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events"]
@@ -125,8 +123,26 @@ class GoogleCalendar:
         self.service.events().update(calendarId=CALENDAR_ID, eventId=event["id"], body=event).execute()
         print("Updated Event Description")
 
+"""
+MIT License
 
-if __name__ == "__main__":
-  x = GoogleCalendar()
-  y = x.getEventByName("Vert (1) vs Storster (4)")
-  print(y)
+Copyright (c) 2025 VertUnderscore
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
